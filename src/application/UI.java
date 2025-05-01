db32e435bf1ddc5e385c87638d7d998c26d94c2d
murilosolino <murilo.solino@outlook.com>
2025-05-01 16:30:00 -0300

    Handling Exceptions and clearScreen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,10 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static ChessPosition readChessPosition(Scanner sc){
 
@@ -41,10 +45,6 @@ public class UI {
         } catch (RuntimeException e){
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
         }
-
-
-
-
     }
 
     public static void printBoard(ChessPiece[][] pieces){
